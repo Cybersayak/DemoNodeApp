c6b69a3921b3e6da8a697922cca026ddc86246ad
SAYAK GHOSH <56470816+Cybersayak@users.noreply.github.com>
2024-08-18 14:02:08 +0000

    HTML File adding for routing

--- server.js
+++ server.js
@@ -1,20 +1,38 @@
 import http from "http";
+import fs from "fs/promises";
+import url from "url";
+import path from "path";
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
+//Get current path using es modules
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// these are es modules
+// __filename displays /home/runner/DemoNodeApp/server.js
+// __dirname displays /home/runner/DemoNodeApp
+//  console.log( __dirname ,__filename);
+
+const server = http.createServer(async (req, res) => {
   try {
     //Check if Get request
     if (req.method === "GET") {
+      let filePath;
       if (req.url === "/") {
-        res.writeHead(500, { "Content-Type": "text/html" });
-        res.end("<h1>Home Page </h1>");
+        //Read index.html file
+        filePath = path.join(__dirname, "public", "index.html");
+        //res.writeHead(500, { "Content-Type": "text/html" }); res.end("<h1>Home Page </h1>");
       } else if (req.url === "/about") {
-        res.writeHead(500, { "Content-Type": "text/html" });
-        res.end("<h1>About </h1>");
+        //Read about.html file
+        filePath = path.join(__dirname, "public", "about.html");
+        //res.writeHead(500, { "Content-Type": "text/html" });res.end("<h1>About </h1>");
       } else {
-        res.writeHead(500, { "Content-Type": "text/html" });
-        res.end("<h1>404 Not Found </h1>");
+        throw new Error("Not Found");
       }
+
+      const data = await fs.readFile(filePath);
+      res.setHeader("Content-Type", "text/html");
+      res.write(data);
+      res.end();
     } else {
       throw new Error("Metthod not allowed");
     }
